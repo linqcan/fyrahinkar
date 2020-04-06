f77c8acfbbf7834213ce17c7e7943d1f47ee085b
Erik Lindqvist <eriksmejl@gmail.com>
2020-04-06 14:07:16 +0200

    Add Project and Bucket types

---
+++ src/Types.ts
@@ -0,0 +1,30 @@
+export type BucketContent = {
+  name: string;
+  description: string;
+  amount: number;
+};
+
+export type Horizon = {
+  from: number;
+  to: number;
+};
+
+export type Bucket = {
+  name: string;
+  description: string;
+  wantedAmount: number;
+  horizon: Horizon;
+  contents: BucketContent[];
+};
+
+export enum Currency {
+  SEK = "kr",
+  USD = "USD",
+}
+
+export type Project = {
+  name: string;
+  description: string;
+  currency: Currency;
+  buckets: Bucket[];
+};
