90f7e1bbd428d570aa5eb32c257da8053a26ec0a
Erik Lindqvist <eriksmejl@gmail.com>
2020-04-07 13:04:11 +0200

    Add ability to create project

---
+++ src/StartPage/AddProjectForm.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import { Project, Currency } from "../Types";
+
+type AddProjectFormProps = {
+  onClose: () => void;
+  onSave: (project: Project) => void;
+  isOpen: boolean;
+};
+
+const AddProjectForm = ({ onClose, isOpen, onSave }: AddProjectFormProps) => {
+  const nameField = React.useRef<HTMLInputElement>(null);
+  const descField = React.useRef<HTMLInputElement>(null);
+  const expensesField = React.useRef<HTMLInputElement>(null);
+  const saveProject = () => {
+    const project: Project = {
+      name: "",
+      description: "",
+      currency: Currency.SEK,
+      necessaryExpenses: 0,
+      buckets: [],
+    };
+    if (nameField && nameField.current !== null) {
+      project.name = nameField.current.value;
+    }
+    if (descField && descField.current !== null) {
+      project.description = descField.current.value;
+    }
+    if (expensesField && expensesField.current !== null) {
+      try {
+        project.necessaryExpenses = Number(expensesField.current.value);
+      } catch {
+        //Raise error somehow
+      }
+    }
+    onSave(project);
+  };
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>Skapa ett nytt sparprojekt</DialogTitle>
+      <DialogContent>
+        <form>
+          <FilledTextField
+            id="project-name"
+            inputRef={nameField}
+            label="Namn"
+            required
+          />
+          <FilledTextField
+            id="project-description"
+            label="Beskrivning"
+            multiline
+          />
+          <FilledTextField
+            id="project-necessaryexpenses"
+            label="Nödvändinga utgifter"
+            helperText="För en månad"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">Kr</InputAdornment>,
+            }}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="text" onClick={onClose}>
+          Avbryt
+        </Button>
+        <Button variant="text" onClick={saveProject}>
+          Spara
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const FilledTextField = (props: any): JSX.Element => (
+  <TextField {...props} fullWidth variant="standard" />
+);
+
+export default AddProjectForm;
--- src/StartPage/BucketList.tsx
+++ src/StartPage/BucketList.tsx
@@ -1,28 +1,46 @@
 import React from "react";
-import { getProject } from "../Storage";
+import { getProject, saveProject } from "../Storage";
 import { PrimaryButton } from "../Components";
+import AddProjectForm from "./AddProjectForm";
+import { Project } from "../Types";
+import Typography from "@material-ui/core/Typography";
 
 const BucketList = (): JSX.Element => {
   const project = getProject();
+  const [open, setOpen] = React.useState(false);
+  const showHideModal = () => setOpen(!open);
+  const projectSaved = (project: Project) => {
+    saveProject(project);
+  };
   if (!project) {
     return (
       <div>
-        <PrimaryButton>Starta ett sparprojekt</PrimaryButton>{" "}
+        <PrimaryButton onClick={showHideModal}>
+          Starta ett sparprojekt
+        </PrimaryButton>
+        <AddProjectForm
+          onSave={projectSaved}
+          onClose={showHideModal}
+          isOpen={open}
+        />
       </div>
     );
-  } else {
-    if (project.buckets.length > 0) {
-      return <PrimaryButton>Skapa en hink</PrimaryButton>;
-    } else {
-      return (
+  }
+  return (
+    <div>
+      <Typography variant="h4">{project.name}</Typography>
+      <Typography variant="body1">{project.description}</Typography>
+      {project?.buckets.length === 0 ? (
+        <PrimaryButton>Skapa en hink</PrimaryButton>
+      ) : (
         <div>
           {project.buckets.map((bucket) => (
             <p>`${bucket.name}`</p>
           ))}
         </div>
-      );
-    }
-  }
+      )}
+    </div>
+  );
 };
 
 export default BucketList;
